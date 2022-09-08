c3911785506e13bc41bbeef153a3b9c7c3f60b32
Benziza <benizizamohamed@gmail.com>
2022-09-08 17:27:58 +0100

    Todo model

---
+++ BackEnd/src/main/java/com/example/backend/models/Todo.java
@@ -0,0 +1,74 @@
+//Let’s now create the Todo model which will be mapped to a Document in the mongodb database.
+package com.example.backend.models;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Document(collection="todos")
+@JsonIgnoreProperties(value = {"createdAt"}, allowGetters = true)//We don’t want clients to send the createdAt value. If they send a value for this field, we’ll simply ignore it.
+public class Todo {
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max=100)
+    @Indexed(unique=true)//This creates a unique index on title field.
+    private String title;
+
+    private Boolean completed = false;
+
+    private Date createdAt = new Date();
+
+    public Todo() {
+        super();
+    }
+
+    public Todo(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Todo[id=%s, title='%s', completed='%s']",
+                id, title, completed);
+    }
+}
