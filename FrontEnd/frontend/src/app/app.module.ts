e232c0c4e4c91a998e01b9e5700d41852a91e305
Benziza <benizizamohamed@gmail.com>
2022-09-08 21:01:24 +0100

    add TodoService inside the providers array

--- FrontEnd/frontend/src/app/app.module.ts
+++ FrontEnd/frontend/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
