d6817f21ec6de8c34aab8ea8a51350629be6c3fd
Benziza <benizizamohamed@gmail.com>
2022-09-08 20:54:00 +0100

    Create template for TodoListComponent todo-list.component.html

--- FrontEnd/frontend/src/app/app.module.ts
+++ FrontEnd/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TodoListComponent } from './todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, TodoListComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
